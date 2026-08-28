32e60d19fac24cb197811d055d45ac0d268a8cc6
David Truth <officialdavidtruth@gmail.com>
2026-08-28 13:13:04 +0100

    Add files via upload

--- prisma/migrations/20260828160000_payment_split_subaccount_code/migration.sql
+++ prisma/migrations/20260828160000_payment_split_subaccount_code/migration.sql
@@ -1,1 +1,4 @@
-
+-- Records the subaccount a charge was actually split to at charge time,
+-- so refund clawback logic doesn't have to infer it from the store's
+-- current payout connection state.
+ALTER TABLE "Payment" ADD COLUMN "splitSubaccountCode" TEXT;
